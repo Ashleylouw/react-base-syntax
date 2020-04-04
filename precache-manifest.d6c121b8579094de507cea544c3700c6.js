8f32fc0f5ccabdfe8b456d6df3643eea19cb67d5
ashleylouw <ashley.louw@blackswan.com>
2020-04-04 10:43:47 +0200

    Updates

---
+++ precache-manifest.d6c121b8579094de507cea544c3700c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad8145cd698c0bbae279f611e88e83db",
+    "url": "/react-base-syntax/index.html"
+  },
+  {
+    "revision": "26af8f30c0e8af7a23d4",
+    "url": "/react-base-syntax/static/css/main.52b70b83.chunk.css"
+  },
+  {
+    "revision": "bab20f85deccd1e9c41d",
+    "url": "/react-base-syntax/static/js/2.0db16348.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-base-syntax/static/js/2.0db16348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26af8f30c0e8af7a23d4",
+    "url": "/react-base-syntax/static/js/main.ea3bc40b.chunk.js"
+  },
+  {
+    "revision": "fb183f50c74df76d6867",
+    "url": "/react-base-syntax/static/js/runtime-main.e9e200be.js"
+  }
+]);
